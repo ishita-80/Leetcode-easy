6457b5c3cd2b2bad0c7b0fb2d84f49bf93380af5
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-09 21:06:54 +0530

    Update reversing num.cpp

--- reversing num.cpp
+++ reversing num.cpp
@@ -6,7 +6,7 @@ int main(){
     cin >> n;
     int ans = 0;
     while(n != 0){
-        if(ans>INT32_MAX/10 || ans< INT32_MIN/10){
+        if(ans > INT32_MAX/10 || ans < INT32_MIN/10){
             ans = 0;
             break;
         }
