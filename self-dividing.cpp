6a82e27be53eb46b316f7dc9eaf44bdefb408bf7
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-09 21:05:20 +0530

    Create self-dividing.cpp

---
+++ self-dividing.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+    vector<int>ans;
+    for(int num= left;num<=right;num++){
+        if(num%10==0){
+        continue;}
+        int temp=num;
+        int flag=1;
+
+    
+    while( temp>0){
+        int digit = temp%10;
+        if(digit==0 || num%digit!=0){
+            flag=0;
+            break;
+        }
+        temp/=10;
+    }
+    if(flag){
+        ans.push_back(num);
+    }
+
+    }
+      return ans;
+    }
+  
+    
+};
