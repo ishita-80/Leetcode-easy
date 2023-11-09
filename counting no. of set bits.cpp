dc53fc086130aa047f228d21c7c2fbc05f49891b
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-09 17:48:42 +0530

    Create counting no. of set bits.cpp

---
+++ counting no. of set bits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    int m=n;
+    int mask=0;
+    while(m!=0){
+        mask=(mask<<1)|1;
+
+        m=m>>1;
+
+    }
+int ans = ~n&mask;
+cout<< ans << endl;
+}
