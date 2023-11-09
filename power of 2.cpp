e895a718d60d276cbec83dedd6bc7b15ef8b9b25
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-09 21:12:41 +0530

    Update power of 2.cpp

--- power of 2.cpp
+++ power of 2.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n;
     int ans=1;
     for(int i=0 ;i <= 30; i++){
-        if(ans==n){
+        if(ans == n){
             cout<<"true";
         }
         if(ans<INT32_MAX/2||ans>INT32_MIN/2){
