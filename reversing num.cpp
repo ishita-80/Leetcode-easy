5e79685c1d202bab03a3b624f5512527215549a1
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-09 17:53:54 +0530

    Create reversing num.cpp

---
+++ reversing num.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<limits>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int ans=0;
+    while(n!=0){
+        if(ans>INT32_MAX/10 || ans< INT32_MIN/10){
+            ans=0;
+            break;
+        }
+       int digit=n%10;
+        ans=(ans*10)+digit;
+        n=n/10;
+       
+
+    } cout << ans;
+}
