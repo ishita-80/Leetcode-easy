88c665bcc602d77d1b50676a8a4fa93acfc75ec0
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 16:46:30 +0530

    Create pow(x,n).cpp

---
+++ pow(x,n).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+     long long nn = n;
+     double ans = 1.0;
+     if(nn<0){
+         nn=-nn;
+
+     }
+     while(nn){
+         if(nn%2==1){
+             ans=ans*x;
+             nn=nn-1;
+         }
+         else{
+             x=x*x;
+             nn=nn/2;
+         }
+     }
+     if(n<0){
+         ans=double(1.0)/double(ans);
+         return ans;
+     }
+     else{
+         return ans;
+     }
+    }
+};
