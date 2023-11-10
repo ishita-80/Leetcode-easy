9cf0e6d37f09e3b667e67ade5446f4892cb228a0
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 16:38:21 +0530

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -4,17 +4,17 @@ public:
         int sum;
         int a = 0;
         int b = 1;
-        if(n==0){
+        if(n == 0){
             return a;
 
         }
-        if (n==1){
+        if (n == 1){
             return b;
         }
         
         
-        for (int i = 1 ; i< n ; i++){
-            sum=a+b;
+        for (int i = 1 ; i < n ; i++){
+            sum = a+b;
             a=b;
             b=sum;
         } 
