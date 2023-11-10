caa8d1f650a0be6fcfd9c41e9e93911d40d35ace
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 15:04:54 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int binarySearch(int arr[],int size,int key){
     int start = 0;
-    int end=size-1;
+    int end = size - 1;
     int mid=(start+end)/2;
     while(start<=end){
         if(arr[mid]==key){
