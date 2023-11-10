0f289eb340c892ed60c5e823b61786a0f0c25f85
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 15:15:44 +0530

    Create Happy num.cpp

---
+++ Happy num.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+     
+        while(n>9){
+            long long sq_digi=0;
+            int digit=0;
+            while(n>=1){
+
+                digit=n%10;
+
+                sq_digi += pow(digit,2);
+                n/=10;
+            }
+            n=sq_digi;
+        }
+        //edge point 7 7 is also a happy number
+        if(n==1 || n==7){
+        return true;}
+        return false;
+    }
+};
