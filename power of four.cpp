94b5900b05c7216b9b5208a0780181a2a481741b
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 16:59:20 +0530

    Update power of four.cpp

--- power of four.cpp
+++ power of four.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         int ans=1;
-        for(int i=0;i<=15;i++){
-            if(ans==n){
+        for(int i=0 ; i<=15 ; i++){
+            if(ans == n){
                 return true;
             
             }
-            if(ans<=INT_MAX/4 && ans>=INT_MIN/4){
-                ans=ans*4;
+            if(ans <= INT_MAX/4 && ans >= INT_MIN/4){
+                ans = ans*4;
             }
 
             
