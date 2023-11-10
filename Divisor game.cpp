91542d75515d1d3e3918462a5a5a0f52272f09a2
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 15:08:22 +0530

    Create Divisor game.cpp

---
+++ Divisor game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        int count=0;
+        int x;
+        
+        for (x=1;x<n;x++){
+       while(n>1){
+           while(0<x<n){
+               if(n%x==0){
+                   count++;
+                   n-=x;
+               }
+               break;
+           }
+
+       }}
+       if(count%2==0){
+           return false;
+       }
+       return true;
+    }
+};
