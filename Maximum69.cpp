c8b946c06eab27a0b711cbeb5076b914e37cebe0
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 16:49:43 +0530

    Update Maximum69.cpp

--- Maximum69.cpp
+++ Maximum69.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-       string s=to_string(num);
+       string s = to_string(num);
        for(int i=0; i<s.size();i++){
            if(s[i]=='6'){
                s[i]='9';
