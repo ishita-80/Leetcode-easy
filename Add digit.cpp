a3a008b415c1fc7319ade651805bb46c3a62b416
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2023-11-10 15:18:15 +0530

    Update Add digit.cpp

--- Add digit.cpp
+++ Add digit.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-       long long n=num;
+       long long n = num;
       
-       while(n>9){
-            long long ans=0;
+       while(n > 9){
+            long long ans = 0;
            while(n>0){
            ans+=n%10;
            n/=10;  
